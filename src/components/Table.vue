8170d0095dc61c6498ab593ba6134473ea23e2c9
SirBang <96539031+SirBang@users.noreply.github.com>
2023-12-01 18:45:53 -0500

    CHORE: pagination color

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -26,7 +26,7 @@
           <a href="#" @click="previousPage()" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
         </li>
         <li v-for="page in totalPages" :key="page">
-          <a href="#" @click="goToPage(page)" :class="{ 'bg-blue-500 text-slate-50': page === currentPage }" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{{ page }}</a>
+          <a href="#" @click="goToPage(page)" :class="{ 'bg-blue-600 text-slate-50': page === currentPage }" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{{ page }}</a>
         </li>
         <li>
           <a href="#" @click="nextPage()" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
